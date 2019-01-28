d6e906086ffe6fe72ac10a109684339e3ab8068c
Pyankovekb <pyankovekb@gmail.com>
2019-01-28 11:12:41 +0500

    Updates

---
+++ precache-manifest.ec1faa13e085d8860bf3bc85ab39f2c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "38c23c64648ff4d0ca1d",
+    "url": "/static/js/main.38c23c64.chunk.js"
+  },
+  {
+    "revision": "86daf99a97da35136710",
+    "url": "/static/js/1.86daf99a.chunk.js"
+  },
+  {
+    "revision": "38c23c64648ff4d0ca1d",
+    "url": "/static/css/main.7af5a0ed.chunk.css"
+  },
+  {
+    "revision": "5904c4a280a3212dc52c9f62dff460c1",
+    "url": "/index.html"
+  }
+];
